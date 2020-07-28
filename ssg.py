d6c9a7730729c4aefbf6e252cfb8d46deecaee0d
Omer Dolev <odolev@microsoft.com>
2020-07-28 15:45:15 +0300

    move init to correct file

--- ssg.py
+++ ssg.py
@@ -2,11 +2,9 @@ import typer
 
 from ssg.site import Site
 import ssg.parsers
-from ssg import extensions
 
 
 def main(source="content", dest="dist"):
-    extensions.load_bundled()
     config = {
         "source": source,
         "dest": dest,
